fe3dd9d4dc08e56edf39f4c777233b6f30017e4b
newnakashima <newnakashima2014@gmail.com>
2019-03-24 18:42:09 +0900

    地図が出せた

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,16 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+const L = window.L;
+var mymap = L.map("map").setView([51.505, -0.09], 13);
+L.tileLayer("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox.streets"
+  }
+).addTo(mymap);
 
 export default {
   name: "app",
@@ -17,9 +27,11 @@ export default {
 </script>
 
 <style lang="stylus">
-  div
-    img
-      border solid black 1px
+div
+  img
+    border solid black 1px
+#map
+  height 500px
 </style>
 
 <style lang="scss">
